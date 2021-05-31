7458d219e9c8360c96b1fad71cfddf15ef314fc9
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-31 11:29:24 +0530

    Added:AddressBookUC7.java, filtering

--- Stream.java
+++ Stream.java
@@ -1,10 +1,23 @@
 package com.addressbook.stream;
 
-public class WelcomeJavaStream {
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Stream;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-    System.out.println("Welcome to Java Stream");
-	}
-
+public class AddressBookUC7 {
+	public static void main(String[] args)
+    {
+  
+        // Creating a Stream of Strings
+        Stream<String> stream = Stream.of("Aryan", "Suresh",
+                                          "Shalshi", "Ganesh","Ramesh");
+  
+        // Using Stream filter(Predicate predicate)
+        // to get a stream consisting of the
+        // elements ending with s
+        stream.filter(str -> str.startsWith("S"))
+            .forEach(System.out::println);
+    
+    }
 }
