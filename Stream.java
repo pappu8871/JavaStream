1fda052c62ea06572b9e5f19a54b50c894dd44c1
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-31 11:32:48 +0530

    Added:AddressBookUC-8, Searching

--- Stream.java
+++ Stream.java
@@ -1,23 +1,22 @@
 package com.addressbook.stream;
 
-import java.util.LinkedHashSet;
-import java.util.Scanner;
-import java.util.Set;
 import java.util.stream.Stream;
 
-public class AddressBookUC7 {
-	public static void main(String[] args)
+public class AddressBookUC8 {
+	 // Driver code
+    public static void main(String[] args)
     {
   
         // Creating a Stream of Strings
-        Stream<String> stream = Stream.of("Aryan", "Suresh",
+        Stream<String> stream = Stream.of("Aryan", "Aahan",
                                           "Shalshi", "Ganesh","Ramesh");
   
         // Using Stream filter(Predicate predicate)
         // to get a stream consisting of the
         // elements ending with s
-        stream.filter(str -> str.startsWith("S"))
+        stream.filter(str -> str.endsWith("n"))
             .forEach(System.out::println);
     
     }
 }
+
